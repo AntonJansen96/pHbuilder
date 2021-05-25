c32354a5da8e10d12893cea66be637056aaae28a
Anton Jansen <antonjansen96@gmail.com>
2021-05-25 16:40:54 +0200

    update protein.add_buffer line

--- examples/03_calibrate/calibrate.py
+++ examples/03_calibrate/calibrate.py
@@ -28,7 +28,7 @@ phbuilder.topol.restrain_dihedrals('GLU', [' HA ', ' CA ', ' CB ', ' HB1'], 1, 6
 phbuilder.topol.restrain_dihedrals('ASP', [' HA ', ' CA ', ' CB ', ' HB1'], 1, 60, 0, 10)
 
 phbuilder.protein.add_box(d_boxMargin=2.0)
-phbuilder.protein.add_buffer("../../proteins/buffer.pdb", "../../proteins/buffer.itp")
+phbuilder.protein.add_buffer("../../proteins/buffer.pdb", "../../proteins/buffer.itp", ph_bufqqA=[-0.0656, 0.5328, 0.5328], ph_bufqqB=[-0.8476, 0.4238, 0.4238])
 phbuilder.protein.add_water()
 
 phbuilder.md.energy_minimize()
